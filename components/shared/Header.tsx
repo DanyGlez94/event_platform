03c241da1251736c95ac35463db82958af093c6f
Daniel Gonzalez <daniel_glez94@hotmail.com>
2024-04-17 20:26:23 -0600

    Navbar for mobile and desktop implemented

--- components/shared/Header.tsx
+++ components/shared/Header.tsx
@@ -2,20 +2,32 @@ import { SignedIn, SignedOut, UserButton } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
 import { Button } from '../ui/button';
+import NavItems from './NavItems';
+import MobileNav from './MobileNav';
 
 const Header = () => {
   return (
     <header className="w-full border-b">
       <div className="wrapper flex items-center justify-between">
         <Link href="/" className="w-36">
-          <Image src="/assets/images/logo.svg" alt="Eventify logo" width={128} height={38} />
+          <Image
+            src="/assets/images/logo.svg"
+            alt="Eventify logo"
+            width={128}
+            height={38}
+          />
         </Link>
+
+        <SignedIn>
+          <nav className="md:flex-between hidden w-full max-w-xs">
+            <NavItems />
+          </nav>
+        </SignedIn>
 
         <div className="flex w-32 justify-end gap-3">
           <SignedIn>
-            <Button asChild className="rounded-full" size="lg">
-              <UserButton afterSignOutUrl='/' />
-            </Button>
+            <UserButton afterSignOutUrl="/" />
+            <MobileNav />
           </SignedIn>
           <SignedOut>
             <Button asChild className="rounded-full" size="lg">
---
+++ components/shared/MobileNav.tsx
@@ -0,0 +1,36 @@
+import {
+  Sheet,
+  SheetContent,
+  SheetDescription,
+  SheetHeader,
+  SheetTitle,
+  SheetTrigger,
+} from '@/components/ui/sheet';
+import { Separator } from "@/components/ui/separator"
+import Image from 'next/image';
+import NavItems from './NavItems';
+
+const MobileNav = () => {
+  return (
+    <nav className="md:hidden">
+      <Sheet>
+        <SheetTrigger className="align-middle">
+          <Image
+            src="/assets/icons/menu.svg"
+            alt="Menu"
+            width={24}
+            height={24}
+            className='cursor-pointer'
+          />
+        </SheetTrigger>
+        <SheetContent className='flex flex-col gap-6 bg-white md:hidden'>
+          <Image src="/assets/images/logo.svg" alt="Eventify logo" width={128} height={38} />
+          <Separator className='border border-gray-50' />
+          <NavItems />
+        </SheetContent>
+      </Sheet>
+    </nav>
+  );
+};
+
+export default MobileNav;
