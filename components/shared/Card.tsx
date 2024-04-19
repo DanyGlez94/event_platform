3ef7b79e09743d7dd02e4378a40f460b4a45baf7
Daniel Gonzalez <daniel_glez94@hotmail.com>
2024-04-18 21:48:58 -0600

    actions added to the even.action file, some to use right away and some other will be used in the near future, also, added logic to show edit and delete buttons in case you are the creator of the event

--- components/shared/Card.tsx
+++ components/shared/Card.tsx
@@ -1,7 +1,9 @@
 import { IEvent } from '@/lib/database/models/events.model';
 import { formatDateTime } from '@/lib/utils';
+import { auth } from '@clerk/nextjs';
 import Image from 'next/image';
 import Link from 'next/link';
+import { DeleteConfirmation } from './DeleteConfirmation';
 
 type CardProps = {
   event: IEvent;
@@ -10,6 +12,11 @@ type CardProps = {
 };
 
 const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
+  const { sessionClaims } = auth();
+  const userId = sessionClaims?.userId as string;
+
+  const isEventCreator = userId === event.organizer._id.toString();
+
   return (
     <div className="group relative flex min-h-[380px] w-full max-w-[400px] flex-col overflow-hidden rounded-xl bg-white shadow-md transition-all hover:shadow-lg md:min-h-[438px]">
       <Link
@@ -17,6 +24,20 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
         style={{ backgroundImage: `url(${event.imageUrl})` }}
         className="flex-center flex-grow bg-gray-50 bg-cover bg-center text-grey-500"
       />
+
+      {isEventCreator && !hidePrice && (
+        <div className="absolute right-2 top-2 flex flex-col gap-4 rounded-xl bg-white p-3 shadow-sm transition-all">
+          <Link href={`/events/${event._id}/update`}>
+            <Image
+              src="/assets/icons/edit.svg"
+              alt="edit"
+              width={20}
+              height={20}
+            />
+          </Link>
+          <DeleteConfirmation eventId={event._id} />
+        </div>
+      )}
 
       <Link
         href={`/events/${event._id}`}
@@ -32,18 +53,25 @@ const Card = ({ event, hasOrderLink, hidePrice }: CardProps) => {
             </p>
           </div>
         )}
-        <p className='p-medium-16 p-medium-18 text-grey-500'>
+        <p className="p-medium-16 p-medium-18 text-grey-500">
           {formatDateTime(event.startDateTime).dateTime}
         </p>
-        <p className='p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black'>{event.title}</p>
-        <div className='flex-between w-full '>
-          <p className='p-medium-14 md:p-medium-16 text-grey-600'>
+        <p className="p-medium-16 md:p-medium-20 line-clamp-2 flex-1 text-black">
+          {event.title}
+        </p>
+        <div className="flex-between w-full ">
+          <p className="p-medium-14 md:p-medium-16 text-grey-600">
             {event.organizer.firstName} {event.organizer.lastName}
           </p>
           {hasOrderLink && (
-            <Link href={`/orders?eventId=${event._id}`} className='flex gap-2'>
-              <p className='text-primary-500'>Order Details</p>
-              <Image src='/assets/icons/arrow.svg' alt='search' width={10} height={10} />
+            <Link href={`/orders?eventId=${event._id}`} className="flex gap-2">
+              <p className="text-primary-500">Order Details</p>
+              <Image
+                src="/assets/icons/arrow.svg"
+                alt="search"
+                width={10}
+                height={10}
+              />
             </Link>
           )}
         </div>
