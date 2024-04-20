47ffe395cd93b2f6f0297062b01b15d491fae6b4
Daniel Gonzalez <daniel_glez94@hotmail.com>
2024-04-20 01:08:58 -0600

    Stripe implemented

--- components/shared/Checkout.tsx
+++ components/shared/Checkout.tsx
@@ -1,20 +1,45 @@
-import { IEvent } from '@/lib/database/models/events.model'
-import React from 'react'
+import { IEvent } from '@/lib/database/models/events.model';
 import { Button } from '../ui/button';
+import { loadStripe } from '@stripe/stripe-js';
+import { useEffect } from 'react';
+import { checkoutOrder } from '@/lib/actions/order.actions';
 
-const Checkout = ({ event, userId }: { event: IEvent, userId: string }) => {
+loadStripe(process.env.NEXT_PUBLIC_STRIPE_PUBLISHABLE_KEY!);
 
-  const onCheckOut = async () => {
-    console.log('Checking out');
-  }
+const Checkout = ({ event, userId }: { event: IEvent; userId: string }) => {
+  useEffect(() => {
+    // Check to see if this is a redirect back from Checkout
+    const query = new URLSearchParams(window.location.search);
+    if (query.get('success')) {
+      console.log('Order placed! You will receive an email confirmation.');
+    }
+
+    if (query.get('canceled')) {
+      console.log(
+        'Order canceled -- continue to shop around and checkout when you’re ready.'
+      );
+    }
+  }, []);
+
+  const onCheckout = async () => {
+    const order = {
+      eventTitle: event.title,
+      eventId: event._id,
+      price: event.price,
+      isFree: event.isFree,
+      buyerId: userId,
+    };
+
+    await checkoutOrder(order);
+  };
 
   return (
-    <form action={onCheckOut} method='post'>
-      <Button type='submit' role='link' size='lg' className='button sm:w-fit'>
+    <form action={onCheckout} method="post">
+      <Button type="submit" role="link" size="lg" className="button sm:w-fit">
         {event.isFree ? 'Get Tickets' : 'Buy Ticket'}
       </Button>
     </form>
-  )
-}
+  );
+};
 
-export default Checkout
+export default Checkout;
